83026df42b0dee55bdfa8202ae7660c6b49c99ef
Freelancer Laju <96712468+freelancer-laju@users.noreply.github.com>
2023-03-12 16:36:25 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "500px",
+      md: "768px",
+      lg: "992px",
+    },
+  },
+  plugins: [],
+};
